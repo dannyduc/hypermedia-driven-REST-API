ee447d91a4510e0f477ef67cd27bd6be8604ab14
Danny Nguyen <danny.duc@gmail.com>
2014-08-30 22:53:06 -0700

    status

--- index.js
+++ index.js
@@ -4,28 +4,28 @@ var Datastore = require('nedb');
 var db = {};
 var responder = require('./httpResponder');
 
-var port = process.argv[2] || 3000;
+var port = process.argv[2] || 3050;
 var root = "http://localhost:" + port;
 
 // Connect to an NeDB database
-db.movies = new Datastore({ filename: 'db/movies', autoload: true});
+db.movies = new Datastore({ filename: 'db/movies', autoload: true });
 
 // Add an index
 db.movies.ensureIndex({ fieldName: 'title', unique: true });
 
-// Necessary for accessing POST data via rea.data object
+// Necessary for accessing POST data via req.body object
 app.use(express.bodyParser());
 
-// catch-all route to set global values
+// Catch-all route to set global values
 app.use(function (req, res, next) {
-    res.type('applicaiton/json');
+    res.type('application/json');
     res.locals.respond = responder.setup(res);
     next();
 });
 
 // Routes
 app.get('/', function (req, res) {
-    res.send("The API is working.");
+    res.send('The API is working.');
 });
 
 app.get('/movies', function (req, res) {
@@ -33,29 +33,32 @@ app.get('/movies', function (req, res) {
 });
 
 app.post('/movies', function (req, res) {
+
     if (!req.body.title) {
-        res.status(400);
-        res.json(400, {error:"A title is required to create a new movie."});
+        res.json(400, { error: { message: "A title is required to create a new movie." }});
         return;
     }
-    db.movies.insert({title: req.body.title}, function (err, created) {
+
+    db.movies.insert({ title: req.body.title }, function (err, created) {
         if (err) {
-            res.json(500, err);
+            res.json(500, { error: err });
             return;
         }
+
         res.set('Location', root + '/movies/' + created._id);
         res.json(201, created);
     });
 });
 
 app.get('/movies/:id', function (req, res) {
-    db.movies.findOne({_id: req.params.id}, function (err, result) {
+    db.movies.findOne({ _id: req.params.id }, function (err, result) {
         if (err) {
-            res.json(500, {error: err});
+            res.json(500, { error: err });
             return;
         }
+
         if (!result) {
-            res.json(404, {error: "We did not find a movie with id " + req.params.id});
+            res.json(404, { error: { message: "We did not find a movie with id: " + req.params.id }});
             return;
         }
 
@@ -64,58 +67,38 @@ app.get('/movies/:id', function (req, res) {
 });
 
 app.put('/movies/:id', function (req, res) {
-    db.movies.update({_id: req.params.id},
-            req.body,
-            function (err, num) {
-                res.locals.respond(err, {success: num + " records updated"});
+    db.movies.update({ _id: req.params.id }, req.body, { upsert: false }, function (err, num) {
+
+        if (err) {
+            res.json(500, { error: err });
+            return;
+        }
+
+        if (num === 0) {
+            res.json(400, { error: { message: "No records were updated." }});
+            return;
+        }
+
+        res.send(204);
+        res.json(200, { success: { message: "Sucessfully updated movie with ID " + req.params.id }});
     });
 });
 
-app.delete('/movies/:id', function(req, res) {
-    db.movies.remove({_id:req.params.id}, function (err, num) {
+app.delete('/movies/:id', function (req, res) {
+    db.movies.remove({ _id: req.params.id }, function (err, num) {
         if (err) {
-            res.json(500, {error: err});
+            res.json(500, { error: err });
             return;
         }
+
         if (num === 0) {
-            res.json(404, {error: "We did not find a movie with id " + req.params.id});
+            res.json(404, { error: { message: "We did not find a movie with id: " + req.params.id }});
             return;
         }
 
         res.set('Link', root + '/movies; rel="collection"');
-        res.send(204);  // no content
+        res.send(204);
     });
 });
 
-app.post('/rpc', function(req, res) {
-
-        var body = req.body;
-        var respond = function(err, results) {
-            if (err) {
-                res.send(JSON.stringify(err));
-            } else {
-                res.send(JSON.stringify(results));
-            }
-        };
-
-        res.set('Content-type', 'application/json');
-
-        switch (body.action) {
-            case "getMovies":
-                db.movies.find({}, respond);
-                break;
-            case "addMovie":
-                db.movies.insert({title: body.title}, respond);
-                break;
-            case "rateMovie":
-                db.movies.update({title: body.title}, {
-                    $set: {rating: body.rating}
-                }, function (err, num) {
-                    respond(err, {success: num + " records updated"});
-                });
-                break;
-            default:
-                respond({error:"No action given in request"});
-        }
-    })
-    .listen(port);
+app.listen(port);
